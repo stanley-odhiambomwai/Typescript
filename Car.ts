f643f3b440d9973d7a74ac4559c2a2db62c0c7b9
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-01-28 20:28:29 +0300

    importing vehicle file in car file and creating properties and subpropeties of car with a method

---
+++ Car.ts
@@ -0,0 +1,17 @@
+import { Vehicle } from './Vehicle';
+
+export class Car implements Vehicle {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  start(): void {
+    console.log("Car engine started");
+  }
+}
